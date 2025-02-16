b96e38a36a729a592a69de90bf353bebfcdf78ec
FunRunRushFlush <funrunrush@gmx.de>
2025-02-16 14:08:44 +0100

    init

--- ExerciseTracker.FunRunRushFlush/ExerciseTracker.App/Data/ExerciseDbContext.cs
+++ ExerciseTracker.FunRunRushFlush/ExerciseTracker.App/Data/ExerciseDbContext.cs
@@ -1,7 +1,24 @@
-﻿namespace ExerciseTracker.App.Data
-{
-    public class ExerciseDbContext
-    {
+﻿using ExerciseTracker.App.Data.DbConfiguration;
+using ExerciseTracker.App.Model;
+using Microsoft.EntityFrameworkCore;
 
+
+namespace ExerciseTracker.App.Data;
+
+public class ExerciseDbContext : DbContext
+{
+    public ExerciseDbContext(DbContextOptions options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Excercise> ExcerciseTable { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        ArgumentNullException.ThrowIfNull(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new ExcerciseConfiguration());
     }
 }
--- ExerciseTracker.FunRunRushFlush/ExerciseTracker.App/Data/Repository/IRepository.cs
+++ ExerciseTracker.FunRunRushFlush/ExerciseTracker.App/Data/Repository/IRepository.cs
@@ -1,8 +1,12 @@
 ﻿using ExerciseTracker.App.Model;
 
-namespace ExerciseTracker.App.Data.Repository
+namespace ExerciseTracker.App.Data.Repository;
+
+public interface IRepository<TEntity> where TEntity : Entity
 {
-    public interface IRepository<TEntity> where TEntity : Entity
-    {
-    }
+    ValueTask CreateAsync(TEntity entity, CancellationToken cancellationToken);
+    ValueTask<bool> DeleteAsync(int id, CancellationToken cancellationToken);
+    ValueTask<List<TEntity>> GetAllAsync(CancellationToken cancellationToken);
+    ValueTask<TEntity?> GetByIdAsync(int id, CancellationToken cancellationToken);
+    ValueTask<bool> UpdateAsync(TEntity entity, CancellationToken cancellationToken);
 }
